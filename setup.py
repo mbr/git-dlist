2edb38983cb01d08e62e4d91330e2d398e4c6d1c
Marc Brinkmann <git@marcbrinkmann.de>
2013-06-15 14:04:36 +0200

    Release 0.1.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='git-dlist',
-    version='0.1dev',
+    version='0.1',
     description='Small tool to keep a directory of git repositories in sync.',
     long_description=read('README.rst'),
     author='Marc Brinkmann',
